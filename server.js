7a7e9b7d5e3a87dad0e5545e5ff8bc471d4c7e2a
Kalosa0770 <kalosaelijah3@gmail.com>
2025-10-23 10:26:48 +0200

    new deployment

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use(cors({
         }
         return callback(new Error('Not allowed by CORS'));
     },
-    credentials: true 
+    credentials: true,
+    methods: ('GET', 'POST', 'PUT', 'DELETE')
 }));
 // Routes
 app.use('/api/users', userRoutes);
