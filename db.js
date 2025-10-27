b3cbd1c1627b5eafd7fd61c01d4f8b4e854c5c7e
Kalosa0770 <kalosaelijah3@gmail.com>
2025-10-27 18:51:15 +0200

    added api mailer

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
+// db.js
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-module.exports = () => {
-    try {
-        mongoose.connect(process.env.DB);
-        console.log('Connected to atlas database successfully');
-    } catch (error) {
-        console.log(error);
-        console.log('Could not connect to database');
-    }
+module.exports = async () => {
+  const dbUri = process.env.DB;
+
+  if (!dbUri) {
+    console.error('MongoDB URI is missing! Set process.env.DB');
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to Atlas database successfully');
+  } catch (error) {
+    console.error('Could not connect to database:', error);
+    process.exit(1);
+  }
 };
