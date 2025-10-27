9885ce436f418e029ed107ced6ccab206847ff2f
Kalosa0770 <kalosaelijah3@gmail.com>
2025-10-27 17:31:39 +0200

    added nondmailer

--- middleware/nodemailer.js
+++ middleware/nodemailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com',
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false, // TLS on port 587
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    }
-});
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
 module.exports = transporter;
